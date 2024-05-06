d4ee3c1cfc6783b0a61452091efda7b1c359ad43
kzsaaf3e <kzsaaf3e@github.com>
2024-05-06 15:52:32 +0700

    RMMSM8953 : Adding Dynamic Flags To Device.mk

---
+++ device.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 Team Win Recovery Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
