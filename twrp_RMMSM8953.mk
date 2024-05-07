42de52ddefbab51cdaa7ad881ad888503e54bd9b
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-05-07 12:36:58 +0700

    Update twrp_RMMSM8953.mk

--- twrp_RMMSM8953.mk
+++ twrp_RMMSM8953.mk
@@ -6,10 +6,19 @@
 
 # Release Name
 PRODUCT_RELEASE_NAME := RMMSM8953
+DEVICE_PATH := device/realme/RMMSM8953
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Kernel
 TARGET_KERNEL := prebuilt
 
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from RMMSM8953 device
 $(call inherit-product, device/realme/RMMSM8953/device.mk)
 
